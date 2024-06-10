08412aba694ec4c86614f0fc24e64a36a17ae47e
SytonK <syton98k@gmail.com>
2024-06-10 14:53:54 +0300

    bounded _init

--- bounded value/bounded_value.gd
+++ bounded value/bounded_value.gd
@@ -1,10 +1,15 @@
 class_name BoundedValue
-extends Node
 
 
-@export var min_value: float = 0 : set = _set_min_value
 @export var max_value: float = 0 : set = _set_max_value
+@export var min_value: float = 0 : set = _set_min_value
 @export var value: float = 0 : set = _set_value
+
+
+func _init(max_value_arg: float = 0, min_value_arg: float = 0, value_arg: float = 0):
+	max_value = max_value_arg
+	min_value = min_value_arg
+	value = value_arg
 
 
 func _set_value(new_value: float) -> void:
---
+++ bounded value/example/bounded_value_example.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var bounded_value: BoundedValue = BoundedValue.new(7, -4, 3)
+var bounded_value2: BoundedValue = BoundedValue.new(-3, 5, 8)
